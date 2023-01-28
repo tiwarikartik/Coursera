4b3635f63b41fbf4019bb932e4799bc4c1e02ac7
Kartik Tiwari <fybsccsem1@gmail.com>
2023-01-28 15:28:27 +0530

    Updated lIstening URL

--- static/javascript/chat.js
+++ static/javascript/chat.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const socket = io.connect("http://127.0.0.1:5000");
+    const socket = io.connect();
     let room;
 
     // Displaying Received Messages
