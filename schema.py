8a28043b9f1fa997a88264b62529a6e6fb3db1ce
Kartik Tiwari <fybsccsem1@gmail.com>
2023-02-11 13:32:53 +0530

    Updated Requiremnet.txt with gevent to make app faster

--- schema.py
+++ schema.py
@@ -1,9 +1,10 @@
-from sqlalchemy_schemadisplay import create_schema_graph
-from sqlalchemy import MetaData
+from eralchemy import render_er
 
-graph = create_schema_graph(
-    metadata=MetaData(
-        "postgresql://chatapp_gamm_user:veUjJBD1aGAfgVFRHMHEhn0Bp0g2FLCv@dpg-cf415q6n6mps0qn9faug-a.oregon-postgres.render.com/chatapp_gamm"
-    )
+## Draw from SQLAlchemy base
+render_er(Base, "erd_from_sqlalchemy.png")
+
+## Draw from database
+render_er(
+    "postgresql://chatapp_gamm_user:veUjJBD1aGAfgVFRHMHEhn0Bp0g2FLCv@dpg-cf415q6n6mps0qn9faug-a.oregon-postgres.render.com/chatapp_gamm",
+    "erd_from_sqlite.png",
 )
-graph.write_png("dbschema.png")
