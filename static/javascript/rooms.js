dcfd5113838f72628fcb30aeeb3a52f681384a8b
Kartik Tiwari <fybsccsem1@gmail.com>
2023-03-12 23:07:13 +0530

    Made more Responsive

--- static/javascript/rooms.js
+++ static/javascript/rooms.js
@@ -6,13 +6,36 @@ socket.on("info", (data) => {
 document.querySelectorAll(".contact").forEach((elem) => {
     elem.classList.remove("active");
     elem.onclick = () => {
+        if (window.innerWidth <= 600) {
+            document.querySelector("#sidebar").style.display = "none";
+            document.querySelector("#rightside-panel").style.display = "block";
+        }
+
         document.querySelectorAll(".contact").forEach((notClicked) => {
             notClicked.classList.remove("active");
         });
+
         elem.classList.toggle("active");
 
         let newRoom = elem.querySelector("#names").textContent;
         document.querySelector("#profile").classList.remove("invisible");
+
+        if (window.innerWidth <= 600) {
+            document.querySelector(".back").addEventListener("click", () => {
+                document.querySelector("#rightside-panel").style.display =
+                    "none";
+                document.querySelector("#sidebar").style.display = "block";
+            });
+        } else {
+            document.querySelector(".back").addEventListener("click", () => {
+                document
+                    .querySelector(".input-area")
+                    .classList.add("invisible");
+                document.querySelector("#profile").classList.add("invisible");
+                document.querySelector("#display-messages").innerHTML = "";
+            });
+        }
+
         document
             .querySelectorAll(".input-area")[0]
             .classList.remove("invisible");
@@ -26,9 +49,9 @@ document.querySelectorAll(".contact").forEach((elem) => {
 
         let lastseen = document.querySelector("#last-seen");
         if (elem.querySelectorAll(".available")[0]) {
-            lastseen.innerHTML = `<span class="circle"></span> Active Now`;
+            lastseen.innerHTML = `<span class="circle"></span> Online`;
         } else {
-            lastseen.innerHTML = "Last Seen on ";
+            lastseen.innerHTML = "<span class='circle'></span> Offline";
         }
 
         if (newRoom == room) {
